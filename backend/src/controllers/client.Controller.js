a6f05bc3be3b7ce45675c4e135c82244be6885b2
Ranjana <0203ranjanadevi@gmail.com>
2024-10-26 10:37:15 +0530

    build addreview route and controller

--- backend/src/controllers/client.Controller.js
+++ backend/src/controllers/client.Controller.js
@@ -2,6 +2,8 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { Vehicle } from "../models/Vehicle.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { Review } from "../models/Review.model.js";
+
 
 const viewListing = asyncHandler(async (req, res) => {
     const vehicles = await Vehicle.find({});
@@ -11,9 +13,18 @@ const viewListing = asyncHandler(async (req, res) => {
     return res.status(200).json(new ApiResponse(200, vehicles, "Vehicles are here"));
 });
 
+
+
 const viewVehicle = asyncHandler(async (req, res) => {
-    const {id} = req.body; 
+    // get id from req.body
+    // find vehicle through id
+    //return res
     console.log(req.body);
+    const {id} = req.body; 
+    if(!id){
+        throw new ApiError(400,"please provide vehicle id")
+    }
+ 
     const vehicle = await Vehicle.findById(id);
     if (!vehicle) {
         throw new ApiError(400, "No such vehicle exists");
@@ -21,4 +32,31 @@ const viewVehicle = asyncHandler(async (req, res) => {
     return res.status(200).json(new ApiResponse(200, vehicle, "Vehicle found")); 
 });
 
-export { viewListing, viewVehicle };
+
+const addReview =asyncHandler(async(req,res)=>{
+    //check if user is client or not
+    //check if vehicle exists or not
+    //get review from req.body
+    //add it in review model
+    //return res
+
+    const user=req.user
+    if(user.type!=="Client"){
+        throw new ApiError(409,"You are not a client")
+    }
+    const {id,content,rating}=req.body
+    console.log(req.body)
+    const vehicle =await Vehicle.findById(id)
+    if(!vehicle){
+        throw new ApiError(409,"No such vehicle exists")
+    }
+    if(!rating){
+        throw new ApiError(409,"Please add rating")
+    }
+    const review=await Review.create({client:user._id, vehicle:vehicle._id,content,rating})
+     
+    return res.status(200).json(new ApiResponse(200,review,"Review created"))
+})
+
+
+export { viewListing, viewVehicle, addReview };
--- backend/src/routes/client.routes.js
+++ backend/src/routes/client.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { viewListing, viewVehicle } from "../controllers/client.Controller.js";
-
+import { viewListing, viewVehicle ,addReview} from "../controllers/client.Controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 router.route("/viewListing").get(viewListing);
 router.route("/viewVehicle").get(viewVehicle);
-
+router.route("/addReview").post(verifyJWT,addReview);
 export default router;
