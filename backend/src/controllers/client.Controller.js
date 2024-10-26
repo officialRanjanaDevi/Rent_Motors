c5fab56513fea43d30188572f55905c8183763a6
Ranjana <0203ranjanadevi@gmail.com>
2024-10-26 12:52:33 +0530

    delete review and addWishlist route created

--- backend/src/controllers/client.Controller.js
+++ backend/src/controllers/client.Controller.js
@@ -1,9 +1,10 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { Vehicle } from "../models/Vehicle.model.js";
+import {Wishlist} from "../models/Wishlist.model.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { Review } from "../models/Review.model.js";
-import { ObjectId } from "mongodb";
+import { MongoCryptInvalidArgumentError, ObjectId } from "mongodb";
 
 const viewListing = asyncHandler(async (req, res) => {
     const vehicles = await Vehicle.find({});
@@ -12,8 +13,6 @@ const viewListing = asyncHandler(async (req, res) => {
     }
     return res.status(200).json(new ApiResponse(200, vehicles, "Vehicles are here"));
 });
-
-
 
 const viewVehicle = asyncHandler(async (req, res) => {
     // get id from req.body
@@ -67,11 +66,71 @@ const addReview =asyncHandler(async(req,res)=>{
         throw new ApiError(409,"Please add rating")
     }
     const review=await Review.create({client:user._id, vehicle:vehicle._id,content,rating})
-     
+     if(!review){
+        throw new ApiError(500,"Some error occurred while adding review, please try again")
+     }
     return res.status(200).json(new ApiResponse(200,review,"Review created"))
 })
 
 
+const deleteReview=asyncHandler(async(req,res)=>{
+    // get user details and review id from req.user and req.body
+     // check if review exists or not
+    // check if user is owner of review or not 
+    // delete review from db
+    // return res
+    const user=req.user
+     const {id}=req.body
+     if(user.type!=="Client"){
+        throw new ApiError(409,"You are not authorized to delete the review")
+     }
+     const review=await Review.findById(id)
+     if(!review){
+        throw new ApiError(409,"No such review exists")
+     }
+
+     if(review.client.toString()!==user._id.toString()){
+        throw new ApiError(409,"You are not authorized to delete this review")
+     }
+
+     const deletedReview = await Review.findByIdAndDelete(id)
+     if(!deletedReview){
+        throw new ApiError(500,"Some error occurred while deleting the review, please try again")
+     }
+     res.status(200).json( new ApiResponse(200,deletedReview,"Review deleted successfully"))
+})
+
+const addToWishlist =asyncHandler(async(req,res)=>{
+    // extract user id from req.user
+    // check if user is client or not
+    // check if vehicle exists or not
+    // check if vehicle is already present in client's wishlist or not
+    // add in wishlist db
+    // return res
+
+    const user=req.user
+    const {id}=req.body
+  
+    if(user.type!=="Client"){
+      throw new ApiError(409,"You are unauthorized to add in wishlist.")
+    }
+
+    const vehicle = await Vehicle.findById(id)
+    if(!vehicle){
+        throw new ApiError(409,"No such vehicle exists")
+    }
+
+    const existedWish =await Wishlist.find({client:user._id, vehicle:vehicle._id})
+    if(existedWish){
+      return res.status(200).json(new ApiResponse(200,existedWish,"Vehicle already exists in your wishlist"))
+    }
+
+    const wish=await Wishlist.create({client:user._id,vehicle:vehicle._id})
+    if(!wish){
+        throw new ApiError(500,"Failed to add in Wishlist, please try again")
+    }
+    return res.status(200).json(new ApiResponse(200,wish,"Vehicle added in your wishlist"))
+})
 
 
-export { viewListing, viewVehicle, addReview };
+export { viewListing, viewVehicle, addReview ,deleteReview, addToWishlist};
--- backend/src/routes/client.routes.js
+++ backend/src/routes/client.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { viewListing, viewVehicle ,addReview} from "../controllers/client.Controller.js";
+import { viewListing, viewVehicle ,addReview,deleteReview, addToWishlist} from "../controllers/client.Controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 router.route("/viewListing").get(viewListing);
 router.route("/viewVehicle").get(viewVehicle);
 router.route("/addReview").post(verifyJWT,addReview);
+router.route("/deleteReview").delete(verifyJWT,deleteReview);
+router.route("/addWishlist").post(verifyJWT,addToWishlist);
+
 export default router;
