4f68e28ed6d518f5d2c082b23c6def2e5e984d92
Ranjana <0203ranjanadevi@gmail.com>
2024-11-17 23:03:09 +0530

    updated app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 const app = express();
+app.use(cookieParser());
 
 app.use(cors({
   origin: ['http://localhost:5174'], 
@@ -10,7 +11,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+
 app.use(express.static("public"));
 
 
