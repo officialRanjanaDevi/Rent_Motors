b8fa9b86bc389ade4c96fda6533a428587ee9eb9
Ranjana <0203ranjanadevi@gmail.com>
2024-11-18 11:46:05 +0530

    updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ dotenv.configDotenv({
   path: "./env",
 });
 
-import connectDB from "./db/index.js";
+import connectDB from "../src/db/index.js";
 const port = process.env.PORT || 3000;
 
 connectDB()
