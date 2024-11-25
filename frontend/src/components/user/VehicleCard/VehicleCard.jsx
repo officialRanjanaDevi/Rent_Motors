41aca5bdc0210a89f1c833b339dd37d6ce9fae36
Ranjana <0203ranjanadevi@gmail.com>
2024-11-25 15:58:34 +0530

    updated

--- frontend/src/components/user/VehicleCard/VehicleCard.jsx
+++ frontend/src/components/user/VehicleCard/VehicleCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect} from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
